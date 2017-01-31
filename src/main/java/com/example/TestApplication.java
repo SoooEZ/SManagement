00587eb3576aed9bed31ea0ca4a3f277a7cf1622
wonderwrz0606 <wonderwrz0606@gmail.com>
2017-01-31 14:20:50 -0500

    First commit

---
+++ src/main/java/com/example/TestApplication.java
@@ -0,0 +1,15 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by sam on 1/31/17.
+ */
+@SpringBootApplication
+public class TestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestApplication.class, args);
+    }
+}
