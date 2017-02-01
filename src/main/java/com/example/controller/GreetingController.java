adaf27effd01c01593868712447f9bd579e6bc85
wonderwrz0606 <wonderwrz0606@gmail.com>
2017-02-01 00:36:38 -0500

    Modified greeting API

--- src/main/java/com/example/controller/GreetingController.java
+++ src/main/java/com/example/controller/GreetingController.java
@@ -1,6 +1,7 @@
 package com.example.controller;
 
 import com.example.model.Greeting;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,8 +16,8 @@ public class GreetingController {
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
-    @RequestMapping("/greeting")
-    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+    @RequestMapping("/greeting/{name}")
+    public Greeting greeting(@PathVariable(value = "name", required = true) String name) {
         return new Greeting(counter.incrementAndGet(),
                 String.format(template, name));
     }
